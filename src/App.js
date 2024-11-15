324770ef4267ca608bb8405877f11eb8118e49c4
aydenmrt <pro@arpm.dev>
2024-11-15 01:43:07 +0100

    Modification localhost 8080 et indentation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
-import { Navbar, Nav, Button, Container, Card, Row, Col } from "react-bootstrap";
+import React, {useState} from 'react';
+import {Navbar, Nav, Button, Container, Card, Row, Col} from "react-bootstrap";
 import './App.css';
 import useSWR from "swr";
 import logo from './Logo_t_shirt_bis.png';
@@ -24,7 +24,7 @@ const categoryLabels = {
 function App() {
     const [selectedCategory, setSelectedCategory] = useState(null);
     const [selectedProduct, setSelectedProduct] = useState(null);
-    const { data: categories, error: categoriesError } = useSWR('http://localhost:8090/categories', fetcher);
+    const {data: categories, error: categoriesError} = useSWR('http://localhost:8080/categories', fetcher);
 
     return (
         <div className="background">
@@ -38,7 +38,7 @@ function App() {
             >
                 <div className="header-overlay"></div>
                 <div className="header-content">
-                    <img src={logo} alt="Logo" className="logo" />
+                    <img src={logo} alt="Logo" className="logo"/>
                     <h1 className="site-title">T-SHIRT SHOP</h1>
                 </div>
             </header>
@@ -46,11 +46,12 @@ function App() {
             {/* Navbar centrée */}
             <Navbar bg="dark" variant="dark" expand="lg" className="main-navbar">
                 <Container className="justify-content-center">
-                    <Navbar.Brand onClick={() => setSelectedCategory(null)} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
-                        <i className="bi bi-house-heart" style={{ fontSize: '1.5rem', marginRight: '8px' }}></i>
+                    <Navbar.Brand onClick={() => setSelectedCategory(null)}
+                                  style={{cursor: 'pointer', display: 'flex', alignItems: 'center'}}>
+                        <i className="bi bi-house-heart" style={{fontSize: '1.5rem', marginRight: '8px'}}></i>
                         HOME
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
                         <Nav>
                             {categoriesError ? (
@@ -85,27 +86,30 @@ function App() {
                         setSelectedProduct={setSelectedProduct}
                     />
                 ) : selectedCategory && selectedProduct ? (
-                    <ProductDetails productId={selectedProduct} setSelectedProduct={setSelectedProduct} />
+                    <ProductDetails productId={selectedProduct} setSelectedProduct={setSelectedProduct}/>
                 ) : (
-                    <HomeScreen categories={categories} setSelectedCategory={setSelectedCategory} setSelectedProduct={setSelectedProduct}/>
+                    <HomeScreen categories={categories} setSelectedCategory={setSelectedCategory}
+                                setSelectedProduct={setSelectedProduct}/>
                 )}
             </main>
-            <Footer />
+            <Footer/>
         </div>
     );
 }
 
-function HomeScreen({ categories, setSelectedCategory, setSelectedProduct }) {
-    const { data, error } = useSWR('http://localhost:8090/products', fetcher);
- 
+function HomeScreen({categories, setSelectedCategory, setSelectedProduct}) {
+    const {data, error} = useSWR('http://localhost:8080/products', fetcher);
+
     if (error) return <div>Échec du chargement des catégories</div>;
     if (!data) return <div>Chargement des catégories...</div>;
     return (
         <div className="home-screen text-center my-4">
             <h2 className="futuristic-heading">Bienvenue sur le projet T-Shirt Shop !</h2>
             <p className="futuristic-paragraph">
-                Ce site a été réalisé dans le cadre du cours de Projet d'Intégration. Découvrez notre sélection de t-shirts et explorez les différentes catégories disponibles. 
-                Ce projet utilise React pour le front-end, Spring Boot pour le back-end, et MySQL pour la gestion de données.
+                Ce site a été réalisé dans le cadre du cours de Projet d'Intégration. Découvrez notre sélection de
+                t-shirts et explorez les différentes catégories disponibles.
+                Ce projet utilise React pour le front-end, Spring Boot pour le back-end, et MySQL pour la gestion de
+                données.
             </p>
             <Row className="justify-content-center">
                 {categories && categories.map((category) => (
@@ -121,54 +125,54 @@ function HomeScreen({ categories, setSelectedCategory, setSelectedProduct }) {
                 ))}
             </Row>
             <Row>
-                
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
 
-{/* Message si aucun produit n'est disponible */}
+                <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px'}}>
 
-{data.length === 0 ? (
-<p>Aucun produit disponible dans le shop.</p>
+                    {/* Message si aucun produit n'est disponible */}
 
-) : (
+                    {data.length === 0 ? (
+                        <p>Aucun produit disponible dans le shop.</p>
 
-    data.map((selectedProduct) => (
-<div
+                    ) : (
 
-            key={selectedProduct.idProduct}
+                        data.map((selectedProduct) => (
+                            <div
 
-            onClick={() => setSelectedProduct(selectedProduct.idProduct)}
+                                key={selectedProduct.idProduct}
 
-            style={{ cursor: 'pointer', textAlign: 'center' }}
->
+                                onClick={() => setSelectedProduct(selectedProduct.idProduct)}
 
-            {/* Miniature du produit avec infobulle */}
-<img
+                                style={{cursor: 'pointer', textAlign: 'center'}}
+                            >
 
-                src={selectedProduct.imageURL || '/placeholder.webp'}
+                                {/* Miniature du produit avec infobulle */}
+                                <img
 
-                alt={selectedProduct.productName}
+                                    src={selectedProduct.imageURL || '/placeholder.webp'}
 
-                title={selectedProduct.productName}
+                                    alt={selectedProduct.productName}
 
-                style={{ width: '100px', height: '100px', objectFit: 'cover', borderRadius: '8px' }}
+                                    title={selectedProduct.productName}
 
-            />
-</div>
+                                    style={{width: '100px', height: '100px', objectFit: 'cover', borderRadius: '8px'}}
 
-    ))
+                                />
+                            </div>
 
-)}
-</div>
+                        ))
+
+                    )}
+                </div>
 
             </Row>
-            
+
 
         </div>
     );
 }
 
-function CategoryPage({ category, setSelectedProduct }) {
-    const { data, error } = useSWR(`http://localhost:8090/categories/${category}/products`, fetcher);
+function CategoryPage({category, setSelectedProduct}) {
+    const {data, error} = useSWR(`http://localhost:8080/categories/${category}/products`, fetcher);
 
     if (error) return <div className="error-message">Échec du chargement des produits</div>;
     if (!data) return <div className="loading-message">Chargement des produits...</div>;
@@ -203,8 +207,8 @@ function CategoryPage({ category, setSelectedProduct }) {
     );
 }
 
-function ProductDetails({ productId, setSelectedProduct }) {
-    const { data, error } = useSWR(`http://localhost:8090/products/${productId}`, fetcher);
+function ProductDetails({productId, setSelectedProduct}) {
+    const {data, error} = useSWR(`http://localhost:8080/products/${productId}`, fetcher);
 
     if (error) return <div>Échec du chargement des détails du produit</div>;
     if (!data) return <div>Chargement des détails du produit...</div>;
@@ -212,17 +216,17 @@ function ProductDetails({ productId, setSelectedProduct }) {
     return (
         <Row className="justify-content-center mt-4">
             <Col xs="12" md="8" lg="6">
-                <Card style={{ boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)', borderRadius: '10px' }}>
+                <Card style={{boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)', borderRadius: '10px'}}>
                     <Card.Header className="text-center">
                         <h2>Détails du produit</h2>
-                    </Card.Header>    
+                    </Card.Header>
                     <Card.Img
                         src={data.imageURL || '/placeholder.webp'}
                         alt={data.productName}
                         style={{
-                            width: '200px', 
-                            height: '200px', 
-                            objectFit: 'cover',  
+                            width: '200px',
+                            height: '200px',
+                            objectFit: 'cover',
                             borderTopLeftRadius: '10px',
                             borderTopRightRadius: '10px',
                             margin: '0 auto',
